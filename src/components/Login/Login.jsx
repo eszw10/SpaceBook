9912147fe3b768466d3396d9c387aed13fc0b8c1
eryc_s.z.w <erycszw10@gmail.com>
2023-02-26 15:33:35 +0700

    add vercel.json and fix some things

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,18 @@
+import { Link } from 'react-router-dom'
 import './Login.css'
+import google from '../../assets/google.png'
+import fb from '../../assets/facebook.png'
 
 const Login = () => {
   return (
     <div className="container flex  min-w-full">
-        <div className="form-container flex flex-col items-center justify-center w-1/2 h-screen gap-6">
+        <div className="form-container flex flex-col items-center justify-center w-1/2 h-screen gap-14">
             <div className="welcome flex flex-col gap-2  w-1/2">
                 <h2 className='font-semibold text-4xl'>Welcome Back</h2>
                 <p className='text-gray-300 mb-4'>login with your data entered during registration</p>
                 <div className="flex flex-col gap-2">
-                    <button className='btn'>Sign in With Google</button>
-                    <button className='btn'>Sign in With facebook</button>
+                    <button className='btn flex justify-center items-center gap-3 font-semibold'><img src={google}/>Sign in with Google</button>
+                    <button className='btn flex justify-center items-center gap-3 font-semibold'><img src={fb} alt="" /> Sign in with facebook</button>
                 </div>
             </div>
             <div className="email m-0 flex flex-col gap-3  w-1/2">
@@ -25,7 +28,7 @@ const Login = () => {
                     <p className='font-bold justify-self-end'>Forgot Password?</p>
                 </div>
                 <button className='btn  bg-gray-500 text-white'>Login</button>
-                <p className='self-center'>Dont have an account? <span className='font-bold'>Sign Up</span></p>
+                <p className='self-center'>Dont have an account? <Link className='font-bold' to={'/signUp'}>Sign Up</Link></p>
             </div>
         </div>
         <div className="img-container w-1/2 bg-gray-400 h-screen"></div>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom"
+import google from '../../assets/google.png'
+import fb from '../../assets/facebook.png'
 
 const SignUp = () => {
   return (
@@ -7,8 +10,8 @@ const SignUp = () => {
                 <h2 className='font-semibold text-4xl'>Welcome</h2>
                 <p className='text-gray-300 mb-4'>Lets Start your journey</p>
                 <div className="flex flex-col gap-2">
-                    <button className='btn'>Sign in With Google</button>
-                    <button className='btn'>Sign in With facebook</button>
+                    <button className='btn flex justify-center items-center gap-3 font-semibold'><img src={google}/>Sign in With Google</button>
+                    <button className='btn flex justify-center items-center gap-3 font-semibold'><img src={fb} alt="" /> Sign in With facebook</button>
                 </div>
             </div>
             <div className="email m-0 flex flex-col gap-3  w-1/2">
@@ -28,7 +31,7 @@ const SignUp = () => {
                     <p className='font-bold justify-self-end text-gray-300'>Forgot Password?</p>
                 </div>
                 <button className='btn  bg-gray-500 text-white'>Sign Up</button>
-                <p className='self-center'>Dont have an account? <span className='font-bold'>Sign Up</span></p>
+                <p className='self-center'>Already have an account? <Link className='font-bold' to={'/login'}>Login</Link></p>
             </div>
         </div>
         <div className="img-container w-1/2 bg-gray-400 h-screen"></div>
