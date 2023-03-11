a5f3672eee8f8e973540f635b3bae0da483fec9e
eryc_s.z.w <erycszw10@gmail.com>
2023-03-11 14:54:06 +0700

    fix search page

--- src/pages/Home/Pages/Section1.jsx
+++ src/pages/Home/Pages/Section1.jsx
@@ -6,7 +6,7 @@ const Section1 = () => {
         <div className="open lg:w-[37%] flex flex-col items-center lg:items-start gap-3">
             <h2 className='font-semibold text-3xl text-grey text-center lg:text-left'>Membantu anda menemukan dan memesan Coworking Space terlengkap</h2>
             <p className='text-[#858585] text-center lg:text-left'>Menyediakan info terlengkap dari harga, kapasitas, dan lokasi Coworking Space</p>
-            <Link type='button' className='lg:self-start btn-orange w-[170px] text-center lg:mt-5' to={window.localStorage.getItem('token')?'/dashboard':'/login'}>Cari Tempat</Link>
+            <Link type='button' className='lg:self-start btn-orange w-[170px] text-center lg:mt-5' to={window.localStorage.getItem('token')?'/cari':'/login'}>Cari Tempat</Link>
         </div>
         <div className="hidden lg:block img-container">
             <img src='/assets/images/landing.png' />
